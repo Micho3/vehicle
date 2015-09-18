5912646c786e783d2d53f2ac153ab9dededa30cf
shepeigang <shepeigang@gaosiedu.com>
2015-09-18 18:46:14 +0800

    adad

--- application/config/common_url.php
+++ application/config/common_url.php
@@ -4,15 +4,17 @@
 $config['common_url']['index'] = "index/index";
 //登陆控制器
 $config['common_url']['login'] = "login/index";
+//录入起始index
+$config['common_url']['insert'] = "insert/index";
 //录入第一步地址
-$config['common_url']['function']['insertStepOne'] = INDEX."insert/insertStepOne";
+$config['common_url']['insertFun']['insertStepOne'] = INDEX."insert/insertStepOne";
 //录入第二步地址
-$config['common_url']['function']['insertStepTwo'] = INDEX."insert/insertStepTwo";
+$config['common_url']['insertFun']['insertStepTwo'] = INDEX."insert/insertStepTwo";
 //
-$config['common_url']['function']['insertUser'] = INDEX."insert/insertUser";
+$config['common_url']['insertFun']['insertUser'] = INDEX."insert/insertUser";
 //获取地区码地址
-$config['common_url']['function']['getAreaCode'] = INDEX."insert/getAreaCode";
-//搜索地址
-$config['common_url']['function']['search'] = INDEX."search/index";
+$config['common_url']['insertFun']['getAreaCode'] = INDEX."insert/getAreaCode";
+//搜索起始index
+$config['common_url']['search'] = "search/index";
 //维护地址
-$config['common_url']['function']['maintenance'] = INDEX."maintenance/index";
+$config['common_url']['maintenance'] = "maintenance/index";
